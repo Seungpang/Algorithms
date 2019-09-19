ebb38e391694d06e0853a8e695b0d11949a5898e
seungpang <obey1342@gmail.com>
2019-09-19 14:45:32 +0900

    LC #20 Valid Parentheses

---
+++ Java/src/leetcode/Valid_Parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    //(() 
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+        for(char c:arr){
+            if(c == '(' || c== '{' ||c=='['){
+                stack.push(c);
+            }else if(c==')'){
+                if(stack.size()==0||stack.pop()!='('){
+                    return false;
+                }
+            }else if(c=='}'){
+                if(stack.size()==0||stack.pop()!='{'){
+                    return false;
+                }
+            }else if(c==']'){
+                if(stack.size()==0||stack.pop()!='['){
+                    return false;
+                }
+            }else{
+                return false;
+            }    
+        }
+        return stack.size()==0;
+    }
+}
