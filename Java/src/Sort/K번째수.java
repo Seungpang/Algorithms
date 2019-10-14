d51eb512c5d71d012f752d57b695868cd356f1fd
seungpang <obey1342@gmail.com>
2019-10-14 23:55:22 +0900

    정렬

---
+++ Java/src/Sort/K번째수.java
@@ -0,0 +1,28 @@
+package Sort;
+import java.util.*;
+
+public class K번째수 {
+    public static void main(String[] args) {
+        K번째수 sol = new K번째수();
+
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands ={{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+
+
+        System.out.print(sol.solution(array, commands));
+    }
+
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int k = 0;
+        for(int[] command : commands){
+            ArrayList<Integer> arr = new ArrayList<>();
+            for(int i = command[0]-1 ; i < command[1] ; i++){
+                arr.add(array[i]);
+            }
+            Collections.sort(arr);
+            answer[k++] = arr.get(command[2]-1);
+        }
+        return answer;
+    }
+}
---
+++ Java/src/Sort/가장_큰_수.java
@@ -0,0 +1,28 @@
+package Sort;
+
+import java.util.Arrays;
+
+public class 가장_큰_수 {
+    public static void main(String[] args) {
+        가장_큰_수 sol = new 가장_큰_수();
+
+        int[] numbers = {6, 10, 2};
+        System.out.println(sol.solution(numbers));
+    }
+
+    public static String solution(int[] numbers) {
+        String[] arr = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++)
+            arr[i] = String.valueOf(numbers[i]);
+
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if(arr[0].equals("0")) return "0";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++)
+            sb.append(arr[i]);
+
+        return sb.toString();
+    }
+}
