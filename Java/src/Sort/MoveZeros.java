5f9d8cdc1e6be01b6ca152e4f424d5a6cddcf2b4
seungpang <obey1342@gmail.com>
2020-06-15 21:53:23 +0900

    MoveZeros

---
+++ Java/src/Sort/MoveZeros.java
@@ -0,0 +1,25 @@
+package Sort;
+
+public class MoveZeros {
+    public static void main(String[] args) {
+
+        int[] nums = {0, 3, 2, 0, 8, 5};
+        int index=0;
+
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] !=0){
+                nums[index] = nums[i];
+                index++;
+            }
+
+        }
+        while (index<nums.length){
+            nums[index] = 0;
+            index++;
+        }
+
+        for(int i=0; i<nums.length; i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
