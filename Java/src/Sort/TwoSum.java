f85af8c38a3b3a47dc254be4f99ca336f6c7325a
Seungpang <obey1342@gmail.com>
2020-06-17 18:59:10 +0900

    Add TowSum

---
+++ Java/src/Sort/TwoSum.java
@@ -0,0 +1,31 @@
+package Sort;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        TwoSum a = new TwoSum();
+        int[] result = a.Solution(nums, target);
+        for(int i : result)
+            System.out.println(i);
+    }
+    public int[] Solution(int[] nums,int target ){
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+                int mapValue = map.get(nums[i]);
+                result[0] = mapValue;
+                result[1] = i;
+            } else{
+                map.put(target-nums[i], i);
+            }
+        }
+        return result;
+    }
+}
