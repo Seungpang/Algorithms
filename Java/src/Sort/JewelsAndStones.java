816b9dceab8ab77ae85f78a31b2158a9b4732418
Seungpang <obey1342@gmail.com>
2020-06-25 23:38:58 +0900

    Add JewelsAndStones.java

---
+++ Java/src/Sort/JewelsAndStones.java
@@ -0,0 +1,25 @@
+package Sort;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class JewelsAndStones {
+    public static void main(String[] args) {
+        String J = "aA", S = "aAAbbbb";
+        System.out.println(solution(J,S));
+    }
+    public static int solution(String jew, String stone) {
+        Set<Character> set = new HashSet<>();
+        for(char jewel : jew.toCharArray()){
+            set.add(jewel);
+        }
+
+        int count=0;
+        for(char stoneChar : stone.toCharArray()) {
+            if(set.contains(stoneChar)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
