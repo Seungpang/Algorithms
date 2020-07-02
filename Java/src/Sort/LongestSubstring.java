54289a0959a8f48f8dc89729dae6149be3021995
Seungpang <obey1342@gmail.com>
2020-07-02 23:36:03 +0900

    Add LongestSubString With At Most Two Distinct

---
+++ Java/src/Sort/LongestSubstring.java
@@ -0,0 +1,33 @@
+package Sort;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String s = "ccaabbb";
+        System.out.println(solution(s));
+    }
+
+    public static int solution(String s){
+        int start = 0, end = 0, length=0, counter=0;
+        Map<Character, Integer> map = new HashMap<>();
+
+        while (end <s.length()){
+            char endChar = s.charAt(end);
+            map.put(endChar, map.getOrDefault(endChar, 0)+1);
+            if(map.get(endChar)==1) counter++;
+            end++;
+
+            while (counter>2){
+               char startChar = s.charAt(start);
+               map.put(startChar, map.get(startChar)-1);
+               if(map.get(startChar)==0) counter--;
+               start++;
+            }
+
+            length = Math.max(length, end-start);
+        }
+        return length;
+    }
+}
