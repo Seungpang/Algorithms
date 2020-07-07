ab70b3818ed6437613b2024564d3ff6d8e97ca18
Seungpang <obey1342@gmail.com>
2020-07-07 23:17:15 +0900

    Add: GroupAnagrams

---
+++ Java/src/StringAndArray/GroupAnagrams.java
@@ -0,0 +1,36 @@
+package StringAndArray;
+
+import java.util.*;
+
+public class GroupAnagrams {
+
+    public static void main(String[] args) {
+        String[] list = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(list));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs){
+
+        List<List<String>> result = new ArrayList<>();
+        if(strs == null || strs.length==0)
+            return result;
+        Map<String, List<String>> map = new HashMap<>();
+
+        for(String str: strs) {
+            char[] charArray = str.toCharArray();
+            Arrays.sort(charArray);
+
+            String key = String.valueOf(charArray);
+            if(map.containsKey(key)){
+                map.get(key).add(str);
+            }else{
+                List<String> list = new ArrayList<>();
+                list.add(str);
+                map.put(key, list);
+            }
+        }
+        result.addAll(map.values());
+        return result;
+    }
+
+}
