0bcca2be050f38693dd746937c11603b28bd9efd
Seungpang <obey1342@gmail.com>
2020-07-09 21:45:49 +0900

    Add TrappingRainWater

---
+++ Java/src/StringAndArray/TrappingRainWater.java
@@ -0,0 +1,47 @@
+package StringAndArray;
+
+public class TrappingRainWater {
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,2,1,0,1,3,2,1,2,1};
+        System.out.println(solution(nums));
+    }
+
+    public static int solution(int[] height){
+
+        int result = 0;
+        if(height == null || height.length <= 2)
+            return result;
+        int[] left = new int[height.length];
+        int[] right = new int[height.length];
+
+        //left
+        int max = height[0];
+        left[0] = height[0];
+        for(int i=1; i<height.length; i++){
+            if(max < height[i]){
+                left[i]=height[i];
+                max = height[i];
+            }else{
+                left[i] = max;
+            }
+        }
+
+        //right
+        max = height[height.length-1];
+        right[height.length-1] = height[height.length-1];
+        for(int i=height.length-2; i>=0; i--){
+            if(max < height[i]){
+                right[i] = height[i];
+                max = height[i];
+            }else
+                right[i] = max;
+        }
+
+        for(int i=0; i<height.length; i++){
+            result += Math.min(left[i], right[i]) - height[i];
+        }
+
+        return result;
+    }
+}
