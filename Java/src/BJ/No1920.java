c583ec66a9e87d7fbe4f053acd2de895bf21a222
Kim Seunglae <obey1342@gmail.com>
2022-11-23 23:52:59 +0900

    BJ1920

---
+++ Java/src/BJ/No1920.java
@@ -0,0 +1,46 @@
+package BJ;
+//수 찾기
+//이진탐색
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class No1920 {
+
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        final int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+
+        int m = sc.nextInt();
+        for (int i = 0; i < m; i++) {
+            boolean find = false;
+            int target = sc.nextInt();
+
+            int start = 0;
+            int end = arr.length - 1;
+            while (start <= end) {
+                int midIndex = (start + end) / 2;
+                int midValue = arr[midIndex];
+                if (midValue > target) {
+                    end = midIndex - 1;
+                } else if (midValue < target) {
+                    start = midIndex + 1;
+                } else {
+                    find = true;
+                    break;
+                }
+            }
+
+            if (find) {
+                System.out.println(1);
+            } else {
+                System.out.println(0);
+            }
+        }
+    }
+}
