f8275e90886690b9250a4058f10f6b7270c9bc83
Kim Seunglae <obey1342@gmail.com>
2022-12-18 22:23:46 +0900

    LC116

---
+++ Java/src/codingInterview/LC116.java
@@ -0,0 +1,50 @@
+package codingInterview;
+
+public class LC116 {
+
+    public static void main(String[] args) {
+        final LC116 lc116 = new LC116();
+    }
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+
+        Node leftEnd = root;
+        while (leftEnd.left != null) {
+            Node head = leftEnd;
+            while (head!= null) {
+                head.left.next = head.right;
+                if (head.next != null) {
+                    head.right.next = head.next.left;
+                }
+                head = head.next;
+            }
+            leftEnd = leftEnd.left;
+        }
+        return root;
+    }
+
+
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+}
