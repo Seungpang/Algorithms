062f62510afd6886da93aea56f9e34cb9b792ac5
Kim Seunglae <obey1342@gmail.com>
2023-01-15 23:34:14 +0900

    LC876

---
+++ Java/src/codingInterview/LC876.java
@@ -0,0 +1,16 @@
+package codingInterview;
+
+import codingInterview.LC23.ListNode;
+
+public class LC876 {
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
