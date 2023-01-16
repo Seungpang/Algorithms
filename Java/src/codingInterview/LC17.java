081c35dfe7715990747326c24a2c7893f3ae7ffb
Kim Seunglae <obey1342@gmail.com>
2023-01-16 23:49:04 +0900

    LC17

---
+++ Java/src/codingInterview/LC17.java
@@ -0,0 +1,44 @@
+package codingInterview;
+
+import codingInterview.LC23.ListNode;
+
+public class LC17 {
+
+    // TC: O(L) SC:O(1)
+    public ListNode removeNthFromEnd1(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        int length = 0;
+        ListNode first = head;
+        while (first != null) {
+            length++;
+            first = first.next;
+        }
+        length -= n;
+        first = dummy;
+        while (length > 0) {
+            length--;
+            first = first.next;
+        }
+        first.next = first.next.next;
+        return dummy.next;
+    }
+
+    // TC: O(L) SC: O(1)
+    public ListNode removeNthFromEnd2(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        ListNode first = dummy;
+        ListNode second = dummy;
+
+        for (int i = 1; i <= n + 1; i++) {
+            first = first.next;
+        }
+
+        while (first != null) {
+            first = first.next;
+            second = second.next;
+        }
+        second.next = second.next.next;
+        return dummy.next;
+    }
+}
--- Java/src/codingInterview/LC876.java
+++ Java/src/codingInterview/LC876.java
@@ -4,7 +4,18 @@ import codingInterview.LC23.ListNode;
 
 public class LC876 {
 
-    public ListNode middleNode(ListNode head) {
+    public ListNode middleNode1(ListNode head) {
+        ListNode[] A = new ListNode[100];
+        int t = 0;
+        while (head != null) {
+            A[t++] = head;
+            head = head;
+        }
+        return A[t / 2];
+    }
+
+    //TC: O(N) SC: O(1)
+    public ListNode middleNode2(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
         while (fast != null && fast.next != null) {
