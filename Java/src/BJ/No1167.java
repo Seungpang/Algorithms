8208984ac4c1ebdaf85e704720779f5abc863f05
Kim Seunglae <obey1342@gmail.com>
2023-03-05 23:43:29 +0900

    트리의 지름 구하기

--- Java/src/BJ/No1167.java
+++ Java/src/BJ/No1167.java
@@ -3,6 +3,7 @@ package BJ;
 //트리의 지름
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -12,42 +13,25 @@ public class No1167 {
 
     static class Edge {
 
-        public int to;
-        public int cost;
+        public int e;
+        public int value;
 
-        Edge(int to, int cost) {
-            this.to = to;
-            this.cost = cost;
+        public Edge(final int e, final int value) {
+            this.e = e;
+            this.value = value;
         }
     }
 
-    public static int[] bfs(int n, List<Edge>[] a, int start) {
-        boolean[] check = new boolean[n + 1];
-        int[] dist = new int[n + 1];
-        Queue<Integer> q = new LinkedList<>();
-        check[start] = true;
-        q.add(start);
-        while (!q.isEmpty()) {
-            int x = q.remove();
-            for (Edge e : a[x]) {
-                int y = e.to;
-                int cost = e.cost;
-                if (check[y] == false) {
-                    dist[y] = dist[x] + cost;
-                    q.add(y);
-                    check[y] = true;
-                }
-            }
-        }
-        return dist;
-    }
+    static List<Edge>[] a;
+    static int[] dist;
+    static boolean[] visited;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        List<Edge>[] a = (List<Edge>[]) new List[n + 1];
+        a = new List[n + 1];
         for (int i = 1; i <= n; i++) {
-            a[i] = new ArrayList<Edge>();
+            a[i] = new ArrayList<>();
         }
 
         for (int i = 1; i <= n; i++) {
@@ -59,21 +43,37 @@ public class No1167 {
                 a[x].add(new Edge(y, z));
             }
         }
-        int[] dist = bfs(n, a, 1);
-        int start = 1;
+        dist = new int[n + 1];
+        visited = new boolean[n + 1];
+        bfs(1);
+        int max = 1;
         for (int i = 2; i <= n; i++) {
-            if (dist[i] > dist[start]) {
-                start = i;
+            if (dist[i] > dist[max]) {
+                max = i;
             }
         }
-        System.out.println(start);
-        dist = bfs(n, a, start);
-        int ans = dist[1];
-        for (int i = 2; i <= n; i++) {
-            if (ans < dist[i]) {
-                ans = dist[i];
+        dist = new int[n + 1];
+        visited = new boolean[n + 1];
+        bfs(max);
+        Arrays.sort(dist);
+        System.out.println(dist[n]);
+    }
+
+    private static void bfs(int index) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(index);
+        visited[index] = true;
+        while (!q.isEmpty()) {
+            int nowNode = q.poll();
+            for (Edge edge : a[nowNode]) {
+                int e = edge.e;
+                int v = edge.value;
+                if (!visited[e]) {
+                    visited[e] = true;
+                    q.add(e);
+                    dist[e] = dist[nowNode] + v;
+                }
             }
         }
-        System.out.println(ans);
     }
 }
