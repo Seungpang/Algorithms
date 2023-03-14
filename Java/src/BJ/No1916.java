b76df40d431add13702c1e52037832198adfa9cb
Kim Seunglae <obey1342@gmail.com>
2023-03-14 23:56:48 +0900

    최소비용 구하기

--- Java/src/BJ/No1916.java
+++ Java/src/BJ/No1916.java
@@ -1,15 +1,11 @@
 package BJ;
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+// 최소비용 구하기
+// 다익스트라
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class No1916 {
 
@@ -18,34 +14,27 @@ public class No1916 {
     static int[] dist;
     static boolean[] visit;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
 
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
+        n = sc.nextInt();
+        m = sc.nextInt();
         list = new ArrayList[n + 1];
         dist = new int[n + 1];
         visit = new boolean[n + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < n; i++) {
             list[i] = new ArrayList<>();
         }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int weight = Integer.parseInt(st.nextToken());
-            list[start].add(new Node(end, weight));
+        for (int i = 0; i < n; i++) {
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+            int cost = sc.nextInt();
+            list[start].add(new Node(end, cost));
         }
-        st = new StringTokenizer(br.readLine());
-        int startIndex = Integer.parseInt(st.nextToken());
-        int endIndex = Integer.parseInt(st.nextToken());
-        bw.write(dijkstra(startIndex, endIndex) + "\n");
-        bw.flush();
-        bw.close();
-        br.close();
+        int startIndex = sc.nextInt();
+        int endIndex = sc.nextInt();
+        System.out.println(dijkstra(startIndex, endIndex));
     }
 
     private static int dijkstra(int start, int end) {
