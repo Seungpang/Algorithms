0fadffc4538fdc28f726e8d687a7b42425af6a02
Kim Seunglae <obey1342@gmail.com>
2023-03-14 17:52:46 +0900

    게임 개발

--- Java/src/BJ/No1516.java
+++ Java/src/BJ/No1516.java
@@ -2,61 +2,57 @@ package BJ;
 //위상 정렬
 //게임 개발
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class No1516 {
 
-    public static void main(String[] args) throws IOException {
-        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        List<List<Integer>> arr = new ArrayList<>();
-
+    public static void main(String[] args) {
+        final Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        List<Integer>[] arr = new ArrayList[n + 1];
         for (int i = 0; i <= n; i++) {
-            arr.add(new ArrayList<>());
+            arr[i] = new ArrayList<>();
         }
 
         int[] indegree = new int[n + 1];
-        int[] selfBuild = new int[n + 1];
+        int[] buildTime = new int[n + 1];
         for (int i = 1; i <= n; i++) {
-            final StringTokenizer st = new StringTokenizer(br.readLine());
-            selfBuild[i] = Integer.parseInt(st.nextToken());
+            buildTime[i] = sc.nextInt();
             while (true) {
-                int preTemp = Integer.parseInt(st.nextToken());
-                if (preTemp == -1) {
+                int command = sc.nextInt();
+                if (command == -1) {
                     break;
                 }
-                arr.get(preTemp).add(i);
+                arr[command].add(i);
                 indegree[i]++;
             }
         }
 
-        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             if (indegree[i] == 0) {
-                queue.offer(i);
+                q.offer(i);
             }
         }
 
         int[] result = new int[n + 1];
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-            for (int next : arr.get(now)) {
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            for (int next : arr[now]) {
                 indegree[next]--;
-                result[next] = Math.max(result[next], result[now] + selfBuild[now]);
+                result[next] = Math.max(result[next], result[now] + buildTime[now]);
                 if (indegree[next] == 0) {
-                    queue.offer(next);
+                    q.offer(next);
                 }
             }
         }
+
         for (int i = 1; i <= n; i++) {
-            System.out.println(result[i] + selfBuild[i]);
+            System.out.println(result[i] + buildTime[i]);
         }
     }
 }
