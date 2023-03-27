df2ddba97380ed2df3872da6f71e4f112f4e6448
Kim Seunglae <obey1342@gmail.com>
2023-03-27 22:35:59 +0900

    숨바꼭질 3

--- Java/src/BJ/No13549_2.java
+++ Java/src/BJ/No13549_2.java
@@ -3,35 +3,33 @@ package BJ;
 //숨바꼭질 3
 
 import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 import java.util.Scanner;
 
 public class No13549_2 {
 
-    public static final int MAX = 100000;
+    public static final int MAX = 100001;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
         int[] dist = new int[MAX];
-        boolean[] check = new boolean[MAX];
-        check[n] = true;
+        Arrays.fill(dist, -1);
+        Deque<Integer> q = new ArrayDeque<>();
+        q.offer(n);
         dist[n] = 0;
-        ArrayDeque<Integer> q = new ArrayDeque<>();
-        q.add(n);
         while (!q.isEmpty()) {
             int now = q.poll();
-            for (int next : new int[]{now*2, now-1, now+1}) {
-                if (next >= 0 && next <MAX) {
-                    if (check[next] == false) {
-                        check[next] = true;
-                        if (now*2 == next) {
-                            q.addFirst(next);
-                            dist[next] = dist[now];
-                        } else {
-                            q.addLast(next);
-                            dist[next] = dist[now] + 1;
-                        }
+            for (int next : new int[]{now * 2, now + 1, now - 1}) {
+                if (next >=0 && next < MAX && dist[next] == -1) {
+                    if (next == now * 2) {
+                        dist[next] = dist[now];
+                        q.addFirst(next);
+                    } else {
+                        dist[next] = dist[now] + 1;
+                        q.addLast(next);
                     }
                 }
             }
