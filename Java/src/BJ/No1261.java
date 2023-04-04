abb3fc05d1f96643d2188f608dbdec547f0e8397
Kim Seunglae <obey1342@gmail.com>
2023-04-04 17:42:38 +0900

    알고스팟

--- Java/src/BJ/No1261.java
+++ Java/src/BJ/No1261.java
@@ -1,72 +1,74 @@
 package BJ;
-//bfs
+//다익스트라
 //알고스팟
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
+import java.util.PriorityQueue;
 import java.util.Scanner;
-
-//class Pair {
-//    int x, y;
-//    Pair(int x, int y) {
-//        this.x = x;
-//        this.y = y;
-//    }
-//}
 
 public class No1261 {
 
-    static int[] dx = {0, 0, -1, 1};
-    static int[] dy = {1, -1, 0, 0};
-
+    static int[][] dirs = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    static int[][] dist;
+    static int[][] arr;
+    static int n, m;
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt();
-        int n = sc.nextInt();
+        m = sc.nextInt();
+        n = sc.nextInt();
         sc.nextLine();
-        int[][] a = new int[n][m];
-        int[][] dist = new int[n][m];
-        for (int i=0; i<n; i++) {
-            String s = sc.nextLine();
-            for (int j=0; j<m; j++) {
-                a[i][j] = s.charAt(j) - '0';
+        arr = new int[n+1][m+1];
+        dist = new int[n+1][m+1];
+        for (int i = 1; i <= n; i++) {
+            String str = sc.nextLine();
+            for (int j = 1; j <= m; j++) {
+                arr[i][j] = str.charAt(j-1) - '0';
             }
         }
-        Queue<Pair> q = new LinkedList<>();
-        Queue<Pair> next_q = new LinkedList<>();
-        q.add(new Pair(0, 0));
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<m; j++) {
-                dist[i][j] = -1;
-            }
-        }
-        dist[0][0] = 0;
-        while (!q.isEmpty()) {
-            Pair p = q.remove();
-            int x = p.x;
-            int y = p.y;
-            for (int k=0; k<4; k++) {
-                int nx = x + dx[k];
-                int ny = y + dy[k];
-                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
-                    if (dist[nx][ny] == -1) {
-                        if (a[nx][ny] == 0) {
-                            dist[nx][ny] = dist[x][y];
-                            q.add(new Pair(nx, ny));
-                        }
-                        if (a[nx][ny] == 1) {
-                            dist[nx][ny] = dist[x][y] + 1;
-                            next_q.add(new Pair(nx, ny));
-                        }
-                    }
 
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], Integer.MAX_VALUE);
+        }
+
+        System.out.println(solve());
+    }
+
+    public static int solve() {
+        PriorityQueue<Point> pq = new PriorityQueue<>();
+        pq.offer(new Point(1, 1, 0));
+        dist[1][1] = 0;
+        while (!pq.isEmpty()) {
+            Point now = pq.poll();
+            if (dist[now.x][now.y] != now.value) {
+                continue;
+            }
+            for (int[] dir : dirs) {
+                int nx = now.x + dir[0];
+                int ny = now.y + dir[1];
+                if (nx > n || ny > m || nx < 1 || ny < 1) {
+                    continue;
+                }
+                if (now.value + arr[nx][ny] < dist[nx][ny]) {
+                    dist[nx][ny] = now.value + arr[nx][ny];
+                    pq.offer(new Point(nx, ny, dist[nx][ny]));
                 }
             }
-            if (q.isEmpty()) {
-                q = next_q;
-                next_q = new LinkedList<>();
-            }
         }
-        System.out.println(dist[n-1][m-1]);
+        return dist[n][m];
+    }
+
+    static class Point implements Comparable<Point> {
+        int x, y, value;
+
+        public Point(final int start, final int end, final int value) {
+            this.x = start;
+            this.y = end;
+            this.value = value;
+        }
+
+        @Override
+        public int compareTo(final Point o) {
+            return this.value - o.value;
+        }
     }
 }
