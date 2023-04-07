0885a6ed33925ac62be9b3c2fb437f04e465d1c0
Kim Seunglae <obey1342@gmail.com>
2023-04-07 17:14:57 +0900

    1로 만들기

--- Java/src/BJ/No1463.java
+++ Java/src/BJ/No1463.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 
 public class No1463 {
 
-    public static int[] dp;
+    public static int[] dp = new int[1000005];
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        dp = new int[n + 1];
         dp[1] = 0;
         for (int i = 2; i <= n; i++) {
             dp[i] = dp[i - 1] + 1;
-            if (n % 2 == 0) {
+            if (i % 2 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 2] + 1);
             }
-            if (n % 3 == 0) {
+            if (i % 3 == 0) {
                 dp[i] = Math.min(dp[i], dp[i / 3] + 1);
             }
         }
