a1c4629639cf687c337111ee34fa953b60b6faf8
Kim Seunglae <obey1342@gmail.com>
2023-04-07 17:10:37 +0900

    1로 만들기

--- Java/src/BJ/No1463.java
+++ Java/src/BJ/No1463.java
@@ -8,35 +8,20 @@ public class No1463 {
 
     public static int[] dp;
 
-    public static int go(int n) {
-        if (n == 1) {
-            return 0;
-        }
-
-        if (dp[n] > 0) {
-            return dp[n];
-        }
-
-        dp[n] = go(n - 1) + 1;
-        if (n%2 == 0) {
-            int temp = go(n / 2) + 1;
-            if (dp[n] > temp) {
-                dp[n] = temp;
-            }
-        }
-        if (n%3 ==0) {
-            int temp = go(n / 3) + 1;
-            if (dp[n] > temp) {
-                dp[n] = temp;
-            }
-        }
-        return dp[n];
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         dp = new int[n + 1];
-        System.out.println(go(n));
+        dp[1] = 0;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (n % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+            if (n % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+        System.out.println(dp[n]);
     }
 }
