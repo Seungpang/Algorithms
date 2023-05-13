f0ef954b43bd8f53a92e34f0cd472cbf403e852d
Seungrae <obey1342@gmail.com>
2023-05-13 17:06:46 +0900

    트리

--- Java/src/BJ/No1068.java
+++ Java/src/BJ/No1068.java
@@ -1,33 +1,50 @@
-//package BJ;
-//// 트리
-//
-//import java.util.Scanner;
-//
-//public class No1068 {
-//
-//    static int answer;
-//    static int[] p;
-//
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//        p = new int[n];
-//        int root = 0;
-//        for (int i = 0; i < n; i++) {
-//            p[i] = sc.nextInt();
-//            if (p[i] == -1) {
-//                root = i;
-//            }
-//        }
-//        int remove = sc.nextInt();
-//
-//        dfs(root)
-//        System.out.println(answer);
-//    }
-//
-//    public static void dfs(int cur) {
-//        for (int c : p) {
-//
-//        }
-//    }
-//}
+package BJ;
+// 트리
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class No1068 {
+
+    static int answer;
+    static int root;
+    static int del;
+    static List<Integer>[] child = new ArrayList[55];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < 55; i++) {
+            child[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < n; i++) {
+            int p = sc.nextInt();
+            if (p == -1) {
+                root = i;
+                continue;
+            }
+            child[p].add(i);
+        }
+        del = sc.nextInt();
+        System.out.println(go(root));
+    }
+
+    static int go(int x) {
+        if (x == del) {
+            return 0;
+        }
+        if (child[x].isEmpty()) {
+            return 1;
+        }
+        if (child[x].size() == 1 && child[x].get(0) == del) {
+            return 1;
+        }
+
+        int result = 0;
+        for (int next : child[x]) {
+            result += go(next);
+        }
+        return result;
+    }
+}
