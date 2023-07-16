776b950a0e303fa3c05fd3e424cec4acbdce4ce6
Seungrae <obey1342@gmail.com>
2023-07-16 22:50:52 +0900

    부분합

--- Java/src/BJ/No1806.java
+++ Java/src/BJ/No1806.java
@@ -1,39 +1,47 @@
 package BJ;
 // 부분합
-//
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class No1806 {
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int s = sc.nextInt();
-		int[] a = new int[n + 1];
-		for (int i = 0; i < n; i++) {
-			a[i] = sc.nextInt();
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		int[] numbers = new int[N + 1];
+		for (int i = 0; i < N; i++) {
+			numbers[i] = Integer.parseInt(st.nextToken());
 		}
-		int i = 0;
-		int j = 0;
-		int sum = a[0];
-		int ans = n + 1;
-		while (i <= j && j < n) {
-			if (sum < s) {
-				j += 1;
-				sum += a[j];
-			} else if (sum == s) {
-				ans = Math.min(j - i + 1, ans);
-				j += 1;
-				sum += a[j];
-			} else if (sum > s) {
-				ans = Math.min(j - i + 1, ans);
-				sum -= a[i];
-				i++;
+		System.out.println(Arrays.toString(numbers));
+
+		int left = 0;
+		int right = 0;
+		int sum = numbers[0];
+		int result = N + 1;
+		while (left <= right && right < N) {
+			if (sum < S) {
+				right++;
+				sum += numbers[right];
+			} else if (sum == S) {
+				result = Math.min(right - left + 1, result);
+				right++;
+				sum += numbers[right];
+			} else if (sum > S) {
+				result = Math.min(right - left + 1, result);
+				sum -= numbers[left];
+				left++;
 			}
 		}
-		if (ans > n) {
-			ans = 0;
+		if (result > N) {
+			result = 0;
 		}
-		System.out.println(ans);
+		System.out.println(result);
 	}
 }
