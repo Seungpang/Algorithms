7415b727a8ec35f0bc150ce66fe1520237891d68
Seungrae <obey1342@gmail.com>
2023-07-18 00:20:30 +0900

    경로 찾기

---
+++ Java/src/BJ/No11403.java
@@ -0,0 +1,41 @@
+package BJ;
+// 경로 찾기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No11403 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] distance = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                distance[i][j] = v;
+            }
+        }
+
+        // 플로이드 워셜
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (distance[i][k] == 1 && distance[k][j] == 1) {
+                        distance[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                System.out.print(distance[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
