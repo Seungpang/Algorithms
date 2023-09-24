3cdf906854be155a490a5282d1a890925a362802
Seungrae <obey1342@gmail.com>
2023-09-24 16:30:46 +0900

    사탕 게임

--- Java/src/BJ/No3085.java
+++ Java/src/BJ/No3085.java
@@ -1,63 +1,72 @@
 package BJ;
+// 사탕 게임
 
 import java.util.Scanner;
 
 public class No3085 {
-
-    static int check(char[][] board) {
-        int n = board.length;
-        int ans = 1;
-        for (int i=0; i<n; i++) {
-            int cnt = 1;
-            for (int j=1; j<n; j++) {
-                if (board[i][j] == board[i][j - 1]) {
-                    cnt += 1;
-                } else {
-                    cnt = 1;
-                }
-                if (ans < cnt) ans = cnt;
-            }
-            cnt = 1;
-            for (int j=1; j<n; j++) {
-                if (board[j][i] == board[j-1][i]) {
-                    cnt += 1;
-                } else {
-                    cnt = 1;
-                }
-                if (ans < cnt) ans = cnt;
-            }
-        }
-        return ans;
-    }
-
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         char[][] board = new char[n][n];
 
-        for (int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             board[i] = sc.next().toCharArray();
         }
 
         int ans = 0;
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                if (j+1 < n) {
-                    char t = board[i][j]; board[i][j] = board[i][j+1]; board[i][j+1] = t;
-                    int temp = check(board);
-                    if (ans < temp) ans = temp;
-                    t = board[i][j]; board[i][j] = board[i][j+1]; board[i][j+1] = t;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (j + 1 < n) {
+                    swap(j, board[i]);
+                    ans = Math.max(check(board), ans);
+                    swap(j, board[i]);
                 }
-                if (i+1 < n) {
-                    char t = board[i][j]; board[i][j] = board[i+1][j]; board[i+1][j] = t;
-                    int temp = check(board);
-                    if (ans < temp) ans = temp;
-                    t = board[i][j]; board[i][j] = board[i+1][j]; board[i+1][j] = t;
+                if (i + 1 < n) {
+                    swap(board, i, j);
+                    ans = Math.max(check(board), ans);
+                    swap(board, i, j);
                 }
             }
         }
         System.out.println(ans);
     }
 
+    private static void swap(int j, char[] board) {
+        char t = board[j];
+        board[j] = board[j + 1];
+        board[j + 1] = t;
+    }
+
+    private static void swap(char[][] board, int i, int j) {
+        char t = board[i][j];
+        board[i][j] = board[i + 1][j];
+        board[i + 1][j] = t;
+    }
+
+    private static int check(char[][] board) {
+        int n = board.length;
+        int ans = 1;
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 1; j < n; j++) {
+                if (board[i][j] == board[i][j - 1]) {
+                    cnt++;
+                } else {
+                    cnt = 1;
+                }
+                ans = Math.max(ans, cnt);
+            }
+            cnt = 1;
+            for (int j = 1; j < n; j++) {
+                if (board[j][i] == board[j - 1][i]) {
+                    cnt++;
+                } else {
+                    cnt = 1;
+                }
+                ans = Math.max(ans, cnt);
+            }
+        }
+        return ans;
+    }
 }
