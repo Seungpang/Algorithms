f52aebfc2810a6c7eae5204161e8914255f8ada5
Seungrae <obey1342@gmail.com>
2024-08-28 21:27:43 +0900

    2차원 배열의 합

--- Java/src/BJ/No2167.java
+++ Java/src/BJ/No2167.java
@@ -14,16 +14,17 @@ public class No2167 {
 
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        int[][] matrix = new int[N + 1][M + 1];
-        int[][] dp = new int[N + 1][M + 1];
+        int[][] arr = new int[N + 1][M + 1];
+        long[][] prefixSum = new long[N + 1][M + 1];
 
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= M; j++) {
-                matrix[i][j] = Integer.parseInt(st.nextToken());
-                dp[i][j] = matrix[i][j] + dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1];
+                arr[i][j] = Integer.parseInt(st.nextToken());
+                prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] - prefixSum[i - 1][j - 1] + arr[i][j];
             }
         }
+
 
         int K = Integer.parseInt(br.readLine());
         for (int k = 0; k < K; k++) {
@@ -33,7 +34,7 @@ public class No2167 {
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
 
-            int sum = dp[x][y] - dp[i - 1][y] - dp[x][j - 1] + dp[i - 1][j - 1];
+            long sum = prefixSum[x][y] - prefixSum[i - 1][y] - prefixSum[x][j - 1] + prefixSum[i - 1][j - 1];
             System.out.println(sum);
         }
     }
