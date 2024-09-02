3f3fca8ceee2d4c5cdbaa3459a8fbbc6aa4e1007
Seungrae <obey1342@gmail.com>
2024-09-02 20:34:29 +0900

    효율적으로 해킹하기

--- Java/src/BJ/No1325.java
+++ Java/src/BJ/No1325.java
@@ -1,64 +1,70 @@
 package BJ;
-
-import java.sql.Array;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.Scanner;
-
 // 효율적으로 해킹하기
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class No1325 {
 
-    static List<Integer>[] a;
+    static int N, M;
+    static List<Integer>[] graph;
+    static int[] count;
     static boolean[] visited;
-    static int[] answer;
 
-    public static void main(String[] args) {
-        final Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        a = new ArrayList[n + 1];
-        for (int i = 1; i <= n; i++) {
-            a[i] = new ArrayList<>();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        graph = new List[N + 1];
+        count = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        for (int i = 0; i < m; i++) {
-            int start = sc.nextInt();
-            int end = sc.nextInt();
-            a[start].add(end);
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            graph[B].add(A);
         }
 
-        answer = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            visited = new boolean[n + 1];
-            bfs(i);
+        int maxCount = 0;
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(visited, false);
+            count[i] = dfs(i);
+            maxCount = Math.max(maxCount, count[i]);
         }
 
-        int maxValue = 0;
-        for (int i = 1; i <= n; i++) {
-            maxValue = Math.max(maxValue, answer[i]);
-        }
-        for (int i = 1; i <= n; i++) {
-            if (answer[i] == maxValue) {
-                System.out.print(i + " ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            if (count[i] == maxCount) {
+                sb.append(i).append(' ');
             }
         }
+
+        System.out.println(sb);
     }
 
-    private static void bfs(int index) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(index);
-        visited[index] = true;
-        while (!q.isEmpty()) {
-            int nowNode = q.poll();
-            for (int i : a[nowNode]) {
-                if (!visited[i]) {
-                    visited[i] = true;
-                    answer[i]++;
-                    q.add(i);
-                }
+    static int dfs(int node) {
+        visited[node] = true;
+        int count = 1;
+
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                count += dfs(next);
             }
         }
+
+        return count;
     }
 }
+
