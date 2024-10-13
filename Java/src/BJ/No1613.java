5a7f2b237995e74184e793c9f52a230cbde623cb
Seungrae <obey1342@gmail.com>
2024-10-13 22:33:45 +0900

    역사

---
+++ Java/src/BJ/No1613.java
@@ -0,0 +1,57 @@
+package BJ;
+// 역사
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class No1613 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        boolean[][] relations = new boolean[N + 1][N + 1];
+
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            relations[a][b] = true;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    if (relations[i][k] && relations[k][j]) {
+                        relations[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        int s = Integer.parseInt(br.readLine());
+        for (int i = 0; i < s; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if (relations[a][b]) {
+                bw.write("-1\n");
+            } else if (relations[b][a]) {
+                bw.write("1\n");
+            } else {
+                bw.write("0\n");
+            }
+        }
+
+        bw.flush();
+    }
+}
