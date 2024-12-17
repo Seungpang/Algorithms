ade2c47ffbec97e12a380ec9a44aa4d2b426bf67
Seungrae Kim <obey1342@gmail.com>
2024-12-17 11:13:39 +0900

    N번째 큰 수

---
+++ Java/src/BJ/No2075.java
@@ -0,0 +1,30 @@
+package BJ;
+// N번째 큰 수
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class No2075 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                pq.offer(Integer.parseInt(st.nextToken()));
+                if (pq.size() > N) {
+                    pq.poll();
+                }
+            }
+        }
+
+        System.out.println(pq.peek());
+    }
+}
