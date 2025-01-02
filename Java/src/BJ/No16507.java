3dab361dea8879209e4f44b94ff39d558da7e65d
Seungrae Kim <obey1342@gmail.com>
2025-01-02 12:16:19 +0900

    어두운 건 무서워

---
+++ Java/src/BJ/No16507.java
@@ -0,0 +1,44 @@
+package BJ;
+// 어두운 건 무서워
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No16507 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int R = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        int Q = Integer.parseInt(st.nextToken());
+
+        int[][] map = new int[R + 1][C + 1];
+        for (int i = 1; i <= R; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= C; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] preSum = new int[R + 1][C + 1];
+        for (int i = 1; i <= R; i++) {
+            for (int j = 1; j <= C; j++) {
+                preSum[i][j] = preSum[i][j - 1] + preSum[i - 1][j] - preSum[i - 1][j - 1] + map[i][j];
+            }
+        }
+
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int r1 = Integer.parseInt(st.nextToken());
+            int c1 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+            int c2 = Integer.parseInt(st.nextToken());
+            int result = preSum[r2][c2] - preSum[r1 - 1][c2] - preSum[r2][c1 - 1] + preSum[r1 - 1][c1 - 1];
+            int count = (r2 - r1 + 1) * (c2 - c1 + 1);
+            System.out.println(result / count);
+        }
+    }
+}
