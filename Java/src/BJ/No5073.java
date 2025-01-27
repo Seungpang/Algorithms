beeaff9564901f013ba984a77427c2663a572b30
Seungrae Kim <obey1342@gmail.com>
2025-01-27 14:44:36 +0900

    삼각형과 세변

---
+++ Java/src/BJ/No5073.java
@@ -0,0 +1,33 @@
+package BJ;
+// 삼각형과 세변
+
+import java.util.Scanner;
+
+public class No5073 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a == 0 && b == 0 && c == 0) {
+                break;
+            }
+
+            int max = Math.max(a, Math.max(b, c));
+            int sum = a + b + c;
+            if (sum - max <= max) {
+                System.out.println("Invalid");
+            } else if (a == b && b == c) {
+                System.out.println("Equilateral");
+            } else if (a == b || a == c || b == c) {
+                System.out.println("Isosceles");
+            } else {
+                System.out.println("Scalene");
+            }
+        }
+    }
+}
