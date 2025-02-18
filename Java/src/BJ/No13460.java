c959225e3236287ad2358e492538d10584e13ae1
Seungrae Kim <obey1342@gmail.com>
2025-02-18 13:52:53 +0900

    구슬 탈출 2

--- Java/src/BJ/No13460.java
+++ Java/src/BJ/No13460.java
@@ -1,152 +1,124 @@
 package BJ;
+// 구슬 탈출 2
 
-import java.util.Scanner;
-
-class Result {
-	boolean moved, hole;
-	int x, y;
-
-	Result(boolean moved, boolean hole, int x, int y) {
-		this.moved = moved;
-		this.hole = hole;
-		this.x = x;
-		this.y = y;
-	}
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class No13460 {
-	static int[] dx = {0, 0, 1, -1};
-	static int[] dy = {1, -1, 0, 0};
-	static final int LIMIT = 10;
 
-	static int[] gen(int k) {
-		int[] dir = new int[LIMIT];
-		for (int i = 0; i < LIMIT; i++) {
-			dir[i] = (k & 3);
-			k >>= 2;
-		}
-		return dir;
-	}
+	static int N, M;
+	static char[][] map;
+	static boolean[][][][] visited;
+	static int holeX, holeY;
+	static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-	static Result simulate(char[][] a, int k, int x, int y) {
-		int n = a.length;
-		int m = a[0].length;
-		if (a[x][y] == '.')
-			return new Result(false, false, x, y);
-		boolean moved = false;
-		while (true) {
-			int nx = x + dx[k];
-			int ny = y + dy[k];
-			if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
-				return new Result(moved, false, x, y);
-			}
-			char ch = a[nx][ny];
-			if (ch == '#') {
-				return new Result(moved, false, x, y);
-			} else if (ch == 'R' || ch == 'B') {
-				return new Result(moved, false, x, y);
-			} else if (ch == '.') {
-				char temp = a[nx][ny];
-				a[nx][ny] = a[x][y];
-				a[x][y] = temp;
-				x = nx;
-				y = ny;
-				moved = true;
-			} else if (ch == 'O') {
-				a[x][y] = '.';
-				moved = true;
-				return new Result(moved, true, x, y);
-			}
-		}
-	}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		map = new char[N][M];
+		visited = new boolean[N][M][N][M];
 
-	static int check(char[][] a, int[] dir) {
-		int n = a.length;
-		int m = a[0].length;
-		int hx = 0, hy = 0;
-		int rx = 0, ry = 0;
-		int bx = 0, by = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (a[i][j] == 'O') {
-					hx = i;
-					hy = j;
-				} else if (a[i][j] == 'R') {
-					rx = i;
-					ry = j;
-				} else if (a[i][j] == 'B') {
-					bx = i;
-					by = j;
+		int redX = 0;
+		int redY = 0;
+		int blueX = 0;
+		int blueY = 0;
+		for (int i = 0; i < N; i++) {
+			String line = br.readLine();
+			for (int j = 0; j < M; j++) {
+				map[i][j] = line.charAt(j);
+				if (map[i][j] == 'R') {
+					redX = i;
+					redY = j;
+					map[i][j] = '.';
+				} else if (map[i][j] == 'B') {
+					blueX = i;
+					blueY = j;
+					map[i][j] = '.';
+				} else if (map[i][j] == 'O') {
+					holeX = i;
+					holeY = j;
 				}
 			}
 		}
-		int cnt = 0;
-		for (int k : dir) {
-			cnt += 1;
-			boolean hole1 = false, hole2 = false;
-			while (true) {
-				Result p1 = simulate(a, k, rx, ry);
-				rx = p1.x;
-				ry = p1.y;
-				Result p2 = simulate(a, k, bx, by);
-				bx = p2.x;
-				by = p2.y;
-				if (p1.moved == false && p2.moved == false) {
-					break;
-				}
-				if (p1.hole)
-					hole1 = true;
-				if (p2.hole)
-					hole2 = true;
-			}
-			if (hole2)
+
+		int result = bfs(redX, redY, blueX, blueY);
+		System.out.println(result);
+	}
+
+	static int bfs(int redX, int redY, int blueX, int blueY) {
+		Queue<State> q = new LinkedList<>();
+		q.offer(new State(redX, redY, blueX, blueY, 0));
+		visited[redX][redY][blueX][blueY] = true;
+
+		while (!q.isEmpty()) {
+			State curr = q.poll();
+			if (curr.count > 10) {
 				return -1;
-			if (hole1)
-				return cnt;
+			}
+			for (int[] dir : dirs) {
+				int[] redRes = move(curr.redX, curr.redY, dir[0], dir[1]);
+				int[] blueRes= move(curr.blueX, curr.blueY, dir[0], dir[1]);
+
+				int nRedX = redRes[0];
+				int nRedY = redRes[1];
+				int redMoves = redRes[2];
+				int nBlueX = blueRes[0];
+				int nBlueY = blueRes[1];
+				int blueMoves = blueRes[2];
+
+				if (nBlueX == holeX && nBlueY == holeY) {
+					continue;
+				}
+				if (nRedX == holeX && nRedY == holeY) {
+					return curr.count + 1;
+				}
+
+				if (nRedX == nBlueX && nRedY == nBlueY) {
+					if (redMoves > blueMoves) {
+						nRedX -= dir[0];
+						nRedY -= dir[1];
+					} else {
+						nBlueX -= dir[0];
+						nBlueY -= dir[1];
+					}
+				}
+
+				if (!visited[nRedX][nRedY][nBlueX][nBlueY]) {
+					visited[nRedX][nRedY][nBlueX][nBlueY] = true;
+					q.offer(new State(nRedX, nRedY, nBlueX, nBlueY, curr.count + 1));
+				}
+			}
 		}
 		return -1;
 	}
 
-	static boolean valid(int[] dir) {
-		int l = dir.length;
-		for (int i = 0; i + 1 < l; i++) {
-			if (dir[i] == 0 && dir[i + 1] == 1)
-				return false;
-			if (dir[i] == 1 && dir[i + 1] == 0)
-				return false;
-			if (dir[i] == 2 && dir[i + 1] == 3)
-				return false;
-			if (dir[i] == 3 && dir[i + 1] == 2)
-				return false;
-			if (dir[i] == dir[i + 1])
-				return false;
+	static int[] move(int x, int y, int dx, int dy) {
+		int moves = 0;
+		while (true) {
+			if (map[x + dx][y + dy] == '#') break;
+			x += dx;
+			y += dy;
+			moves++;
+			if (map[x][y] == 'O') break;
 		}
-		return true;
+		return new int[]{x, y, moves};
 	}
 
-	public static void main(String args[]) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int m = sc.nextInt();
-		String[] map = new String[n];
-		char[][] a = new char[n][m];
-		for (int i = 0; i < n; i++) {
-			map[i] = sc.next();
+	static class State {
+		int redX, redY, blueX, blueY, count;
+
+		public State(final int redX, final int redY, final int blueX, final int blueY, final int count) {
+			this.redX = redX;
+			this.redY = redY;
+			this.blueX = blueX;
+			this.blueY = blueY;
+			this.count = count;
 		}
-		int ans = -1;
-		for (int k = 0; k < (1 << (LIMIT * 2)); k++) {
-			int[] dir = gen(k);
-			if (!valid(dir))
-				continue;
-			for (int i = 0; i < n; i++) {
-				a[i] = map[i].toCharArray();
-			}
-			int cur = check(a, dir);
-			if (cur == -1)
-				continue;
-			if (ans == -1 || ans > cur)
-				ans = cur;
-		}
-		System.out.println(ans);
 	}
 }
